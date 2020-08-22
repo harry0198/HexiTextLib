908a98601058aa7520b7aa99fafd94dbfa8993c6
L1m0n3 <13360351+l1m0n3@users.noreply.github.com>
2020-08-22 13:55:54 +0900

    Use better regex
    StringIndexOutOfBoundsException occurs when passing a string that contains <g> or <gradient> to parseHexString. This will fix that issue.

--- src/main/java/com/haroldstudios/hexitextlib/HexResolver.java
+++ src/main/java/com/haroldstudios/hexitextlib/HexResolver.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class HexResolver {
 
-    private static final Pattern GRADIENT_PATTERN = Pattern.compile("<(gradient|g)(:#([a-fA-F0-9]){6})*>");
+    private static final Pattern GRADIENT_PATTERN = Pattern.compile("<(gradient|g)(:#([a-fA-F0-9]){6})+>");
     private static final Pattern HEX_PATTERN = Pattern.compile("<(#[a-fA-F0-9]{6})>");
 
 
